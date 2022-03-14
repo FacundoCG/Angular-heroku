d0a07ad3b16490a554afebc688ebd092fd8b5b62
Facundo Coronel <coronelfacundo30@gmail.com>
2022-03-14 20:29:39 -0300

    Nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PruebaComponent } from './components/prueba/prueba.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {path: "", component: HomeComponent},
   {path : "/prueba", component: PruebaComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
