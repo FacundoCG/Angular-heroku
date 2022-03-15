2957313f8ec3c8f7d33a7b43efdec890d53d586b
Facundo Coronel <coronelfacundo30@gmail.com>
2022-03-14 22:44:35 -0300

    Trying to fix name of the folder

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/b-inclusive-front'),
+      dir: require('path').join(__dirname, './coverage/angular-heroku'),
       subdir: '.',
       reporters: [
         { type: 'html' },
